bf0b34dd42247881cdfa692ce55f1268579c4510
Dustin H. <dustinholder14@yahoo.com>
2024-01-05 12:01:20 -0600

    Controlled component handling

--- 2.6-2.10/src/App.jsx
+++ 2.6-2.10/src/App.jsx
@@ -4,18 +4,36 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('Insert a name')
+
+  const addPerson = (event) => {
+    event.preventDefault() //Don't refresh the page
+    const personObj = {
+      name: newName,
+    }
+    setPersons(personObj.name)
+    setNewName('')
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" 
+          onClick={addPerson}>
+          Add
+          </button>
         </div>
+        <div>debug: {newName}</div>
       </form>
       <h2>Numbers</h2>
       ...
